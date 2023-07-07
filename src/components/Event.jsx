44cdfaf606af4d52fa0cd1730f97886aaa551691
Masha <nasonkina@yandex.ru>
2023-07-07 13:48:11 +0300

    fix imports

--- src/components/Event.jsx
+++ src/components/Event.jsx
@@ -1,25 +1,31 @@
-import React from "react";
+import { useRef, useEffect } from "react";
 
 export function Event(props) {
-  const ref = React.useRef();
+  const ref = useRef();
 
   const { onSize } = props;
 
-  React.useEffect(() => {
-      const width = ref.current.offsetWidth;
-      const height = ref.current.offsetHeight;
-      if (onSize) {
-          onSize({ width, height });
-      }
+  useEffect(() => {
+    const width = ref.current.offsetWidth;
+    const height = ref.current.offsetHeight;
+    if (onSize) {
+      onSize({ width, height });
+    }
   });
 
-  return <li ref={ref} className={'event' + (props.slim ? ' event_slim' : '')}>
+  return (
+    <li ref={ref} className={"event" + (props.slim ? " event_slim" : "")}>
       <button className="event__button">
-          <span className={`event__icon event__icon_${props.icon}`} role="img" aria-label={props.iconLabel}></span>
-          <h4 className="event__title">{props.title}</h4>
-          {props.subtitle &&
-              <span className="event__subtitle">{props.subtitle}</span>
-          }
+        <span
+          className={`event__icon event__icon_${props.icon}`}
+          role="img"
+          aria-label={props.iconLabel}
+        ></span>
+        <h4 className="event__title">{props.title}</h4>
+        {props.subtitle && (
+          <span className="event__subtitle">{props.subtitle}</span>
+        )}
       </button>
-  </li>;
+    </li>
+  );
 }
